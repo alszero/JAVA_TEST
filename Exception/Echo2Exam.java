265a0163aec1635553176bc074826860738402e2
alszero <alszero05@gmail.com>
2022-09-27 00:03:12 +0900

    Echo2Exam try catch 구문으로 수정

--- Exception/Echo2Exam.java
+++ Exception/Echo2Exam.java
@@ -1,14 +1,36 @@
 package Exception;
 
+import java.io.IOException;
 import java.io.InputStream;
+
+//public class Echo2Exam {
+//    public static void main(String[] args) {
+//        InputStream is = System.in;
+//        while(true){
+//            int i = is.read();
+//            if (i==-1) break;
+//            System.out.println((char)i);
+//        }
+//    }
+//}
+
+//에러메세지
+//D:\JAVA_TEST\Exception\Echo2Exam.java:9:28
+//java: unreported exception java.io.IOException; must be caught or declared to be thrown
+
+// 위에 내용 수정
 
 public class Echo2Exam {
     public static void main(String[] args) {
         InputStream is = System.in;
-        while(true){
-            int i = is.read();
-            if (i==-1) break;
-            System.out.println((char)i);
+        try {
+            while (true) {
+                int i = is.read();
+                if (i == -1) break;
+                System.out.println((char) i);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
